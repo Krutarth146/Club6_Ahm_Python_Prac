b185ab00fea57f6c1be61bf383e549d92ade7908
Krutarth Patel <krutarthpatel146@gmail.com>
2023-07-20 13:55:50 +0530

    List Practice

--- abstraction_13_7.py
+++ abstraction_13_7.py
@@ -5,7 +5,7 @@ from abc import abstractmethod, ABC
 class RBI(ABC):
 
     @abstractmethod
-    def royal():  # balnk Body
+    def royal():  # blank Body
         pass
 
     def depositFD(self): 
@@ -43,7 +43,7 @@ try:
     # r1 = RBI()
     # r1.depositFD()
     print(8/0)
-    
+
 except ZeroDivisionError:
     print("Zero Div. Error")
 
@@ -58,3 +58,71 @@ else:
 
 finally:
     print("Always Executed")
+
+
+l1 =  [(10,90), (232,21), (89,78), (21,22), (3,4)]
+# ans = [(3,4) , ....]
+
+l2 = [1,0,2,3,3,3,32,2,3,31,1,2,3]
+
+# Ans = {1 : [0,10], 2 : [2,7,11] .....}
+
+
+
+l1 =  [(10,90), (232,21), (89,78), (21,22), (3,4)]
+
+for i in range(len(l1)-1): # 5   ---> 0 to 3
+    for j in range(i+1, len(l1)):
+        if l1[i][-1] > l1[j][-1]:
+            # l1[i], l1[j] = l1[j], l1[i]
+            safe = l1[i]
+            l1[i] = l1[j]
+            l1[j] = safe
+
+
+print(l1)
+
+# ------------------------------
+
+def jayraj(tup1):
+    return tup1[-1]
+
+# print(jayraj((30,40,89,45)))
+
+l1.sort(key=jayraj)
+
+print(l1)
+
+# a,b,c = 90,10,20
+
+
+# ---------------------------------------
+
+l2 = [1,0,2,3,3,3,32,2,3,31,1,2,3]
+
+# Ans = {1 : [0,10], 2 : [2,7,11] .....}
+
+dict1 = {}
+
+unique = []
+
+for i in l2:
+    if i not in unique:
+        unique.append(i)
+
+# unique = list(set(l2))
+
+for element in unique:  # i = 1
+    temp = []
+    for ind in range(len(l2)):
+        if element == l2[ind]:
+            temp.append(ind)  # [0,10]
+
+    dict1[element] = temp
+
+print(dict1)   # {1: [0, 10], 0: [1], 2: [2, 7, 11], 3: [3, 4, 5, 8, 12], 32: [6], 31: [9]}
+print()
+print()
+print()
+dict2 = {element : [ind for ind in range(len(l2)) if element == l2[ind]] for element in unique}
+print(dict2)   # 120 Rs.
